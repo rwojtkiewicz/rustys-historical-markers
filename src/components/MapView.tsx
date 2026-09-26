47629a2defeaf2619fb4644fed12c8386ec9e13a
RUSSELL WOJTKIEWICZ <rwojtkiewicz@RUSSELLs-Mac-mini.local>
2026-09-26 11:07:21 -0500

    Fix MapView background by switching to OpenStreetMap standard tiles (no API key required)

--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -177,10 +177,11 @@ export const MapView: React.FC<MapViewProps> = ({
         >
           <MapController center={mapCenter} zoom={zoom} routeBounds={routeBounds} />
 
-          {/* OpenStreetMap Dark Carto Tiles */}
+          {/* OpenStreetMap Standard Tiles (Free & Open, No API Key Required) */}
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            maxZoom={19}
           />
 
           {/* Active Route Line */}
